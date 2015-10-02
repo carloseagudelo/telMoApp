6aa3f8aa4d72115b7788755f790592ee2826139c
Carlos Agudelo <carloskikea@gmail.com>
2015-10-02 10:54:26 -0500

    SE crea y modifica el activity y frame de perfil del motel

---
+++ app/src/main/java/telmoapp/com/telmoapp/motelProfile.java
@@ -0,0 +1,48 @@
+package telmoapp.com.telmoapp;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class motelProfile extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motel_profile);
+        if (savedInstanceState == null) {
+            // Create a fragment
+            motelProfileFragment fragment= new  motelProfileFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(android.R.id.content, fragment,  fragment.getClass().getSimpleName());
+            fragmentTransaction.commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_motel_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/telmoapp/com/telmoapp/motelProfileFragment.java
@@ -0,0 +1,22 @@
+package telmoapp.com.telmoapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class motelProfileFragment extends Fragment {
+
+    public motelProfileFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_motel_profile, container, false);
+    }
+}
