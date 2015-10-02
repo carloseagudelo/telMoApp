1f0f09a6ebf41910a595d3cd9d2807a6ae80b762
Carlos Agudelo <carloskikea@gmail.com>
2015-10-02 11:15:25 -0500

    Se crea el fragment y activity de sexShopProfile ademas de agregar el diseño a la misma

---
+++ app/src/main/java/telmoapp/com/telmoapp/sexShopProfileFragment.java
@@ -0,0 +1,22 @@
+package telmoapp.com.telmoapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class sexShopProfileFragment extends Fragment {
+
+    public sexShopProfileFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sex_shop_profile, container, false);
+    }
+}
