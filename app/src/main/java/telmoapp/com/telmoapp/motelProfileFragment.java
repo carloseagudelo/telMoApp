702497ca6cf5f8ff93c331929fdc2884b2cb2ab2
Carlos Agudelo <carloskikea@gmail.com>
2015-10-25 23:40:10 -0500

    Se corrige el problema de navegacion entre vistas

--- app/src/main/java/telmoapp/com/telmoapp/motelProfileFragment.java
+++ app/src/main/java/telmoapp/com/telmoapp/motelProfileFragment.java
@@ -1,15 +1,19 @@
 package telmoapp.com.telmoapp;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class motelProfileFragment extends Fragment {
+
+    TextView linkcalification;
 
     public motelProfileFragment() {
     }
@@ -17,6 +21,17 @@ public class motelProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_motel_profile, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_motel_profile, container, false);
+
+        linkcalification = (TextView) v.findViewById(R.id.link_calification2);
+
+        linkcalification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent("telmoapp.calification"));
+            }
+        });
+        return v;
     }
 }
